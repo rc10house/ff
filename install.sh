41fac36ba60010eb43bc905ed981899810055448
rthenhaus <rthenhaus@wisc.edu>
2024-05-01 12:01:10 -0500

    small fix

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ os=$1
 
 if [[ $os == "linux" ]]; then 
   curr=$(pwd)
-  echo "alias f=. $pwd/fflin.sh" >> ~/.bash_profile
+  echo "alias f=. $curr/fflin.sh" >> ~/.bash_profile
 elif [[ $os == "mac" ]]; then 
   curr=$(pwd)
-  echo "alias f=. $pwd/ff.sh" >> ~/.bash_profile
+  echo "alias f=. $curr/ff.sh" >> ~/.bash_profile
 else 
   echo "usage: ./install.sh mac or ./installsh linux"
   exit
